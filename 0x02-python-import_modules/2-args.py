a6e0355e56566ff125a1b4b04d180a67d7ad44bd
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-09-09 17:56:53 +0200

    Added 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     from sys import argv
     arguments = len(argv) - 1
 
-    if arguments == 0:
+    if arguments is 0:
         print("{:d} arguments.".format(arguments))
-    elif arguments == 1:
+    elif arguments is 1:
         print("{:d} argument:".format(arguments))
         print("{:d}: {}".format(1, str(argv[1])))
     else:
