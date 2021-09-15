1a851580d97ca02d669ca3470cd34cc851cff0d5
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-09-15 23:27:35 +0200

    Added 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-
-
 def weight_average(my_list=[]):
 
-        if my_list == []:
-                    return 0
+    if my_list == []:
+        return 0
 
-                    sum = 0
-                        divisor = 0
+    sum = 0
+    divisor = 0
 
-                            for i, j in my_list:
+    for i, j in my_list:
 
-                                        sum += (lambda i, j: i * j)(i, j)
+        sum += (lambda i, j: i * j)(i, j)
 
-                                            for i in my_list:
-                                                        divisor += i[1]
+    for i in my_list:
+        divisor += i[1]
 
-                                                            return(sum/divisor)
+    return(sum/divisor)
