84fc6331b3c6e19fb92132578879fce24144cde2
Panashe Rusakaniko <panasherussy10@gmail.com>
2021-09-15 23:26:37 +0200

    Added 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+
+        if my_list == []:
+                    return 0
+
+                    sum = 0
+                        divisor = 0
+
+                            for i, j in my_list:
+
+                                        sum += (lambda i, j: i * j)(i, j)
+
+                                            for i in my_list:
+                                                        divisor += i[1]
+
+                                                            return(sum/divisor)
