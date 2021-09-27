3a8439fcd0ddb5ca4bb17fa79bf765254d9bd244
Panashe Rusakaniko <panasherussy10@gmail.com>
2021-09-27 21:39:09 +0200

    Added 1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        if isinstance(value, int) is True:
+            print('{:d}'.format(value))
+            return True
+        else:
+            raise
+    except:
+        return False
