1912d0dc6aa454987dae75c70adac1c85c9d8c45
Panashe Rusakaniko <panasherussy10@gmail.com>
2021-09-27 22:05:05 +0200

    Added 101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import stderr
+def safe_function(fct, *args):
+    try:
+        results = fct(*args)
+        return results
+    except Exception as err:
+        print('Exception: {}'.format(err), file=stderr)
+        return None
