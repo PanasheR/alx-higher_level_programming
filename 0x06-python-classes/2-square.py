dedc988c8b843790ad959bbc6b3b41d8d1e7b25f
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-09-28 23:20:24 +0200

    Update 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Class Square defines a square"""
+
+
+class Square:
+    """Class Square that defines a square
+    This class has no public attributes
+    """
+
+    def __init__(self, size=0):
+        """ Method to initialize the square object
+        Args:
+             size (int): This defines size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
