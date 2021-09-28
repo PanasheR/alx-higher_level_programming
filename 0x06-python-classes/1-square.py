fd567e9fd8416a6c64dbfbe37e012e17c4f7ffd1
Panashe Rusakaniko <panasherussy10@gmail.com>
2021-09-28 22:50:50 +0200

    Added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """empty class that defines a square"""
+
+    def __init__(self, size):
+    """Args:
+            size (int): length of side of square
+    """
+    self.__size = size
