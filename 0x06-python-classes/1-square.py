4eef1d9f04b930d7f0350be68d358db06ff7cac0
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-09-28 23:17:53 +0200

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
+
 class Square:
     """empty class that defines a square"""
 
     def __init__(self, size):
-    """Args:
+        """Args:
             size (int): length of side of square
-    """
-    self.__size = size
+        """
+        self.__size = size
