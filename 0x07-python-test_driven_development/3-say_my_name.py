bc11bb9d7444ed98a6c5e336d34027b7b755a8a8
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-10-04 22:48:16 +0200

    Added 3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+"""
+This is an example of the say_my_name function.
+>>> say_my_name("John", "Smith")
+John Smith
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """This function prints 2 strings"""
+
+    if isinstance(first_name, str) is False:
+        raise TypeError('first_name must be a string')
+
+    elif isinstance(last_name, str) is False:
+        raise TypeError('last_name must be a string')
+
+    else:
+        print('My name is {} {}'.format(first_name, last_name))
