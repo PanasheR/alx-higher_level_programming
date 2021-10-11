a696cd7edb924df44e4daac2ce24ea04119b4331
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-10-12 00:34:11 +0200

    Update 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+
+
+class BaseGeometry:
+    def area(self):
+        """
+        Public instance method that raises an exception for area that is
+        not implemented
+        """
+
+        raise Exception('area() is not implemented')
