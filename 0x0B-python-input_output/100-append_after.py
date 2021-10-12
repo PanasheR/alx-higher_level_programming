e8d183dab7f92c7df048dda55a50676705a38d99
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-10-12 15:01:28 +0200

    Update 100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
-"""
-function that inserts a line of text to a file, after each line containing a specific string
+
+
+"""This is an example of the append_after function
+>>> append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
+At Holberton School,
+Python is really important,
+But it can be very hard if:
+- You don't get all Pythonic tricks
+- You don't have strong C knowledge.
+
+
+cat append_after_100.txt
+
+Python is really important,
+"C is fun!"
+But it can be very hard if:
+- You don't get all Pythonic tricks
+"C is fun!"
+- You don't have strong C knowledge.
 """
 
 
 def append_after(filename="", search_string="", new_string=""):
-    '''module Search and update
-    '''
-    with open(filename, 'r+') as f:
+
+    """
+    Function that inserts a line of text to a file, after each line containing
+    a specific string
+    """
+    with open(filename, mode='r', encoding='utf-8') as f:
         lines = f.readlines()
-        i = 0
+        f. close()
+
+    with open(filename, mode='w', encoding='utf-8') as f:
         for line in lines:
-            if line.find(search_string) is not -1:
-                lines.insert(i + 1, new_string)
-            i += 1
-        f.seek(0)
-        f.write("".join(lines))
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
+
+        f.close()
