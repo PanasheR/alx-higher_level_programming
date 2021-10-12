36a855788af8639c1594307d2b4244d9c78bca7e
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-10-12 15:08:00 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-''' script that adds all arguments to a Python list, and then save them to a file
-'''
-import json
-import sys
+"""Module 9-add_item
+Adds all arguments to a Python list, and then save them to a file
+"""
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+import sys
+import json
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
+my_list = []
 try:
-    py_list = load_from_json_file(filename)
-except FileNotFoundError:
-    py_list = []
-finally:
-    for el in sys.argv[1:]:
-        py_list.append(str(el))
-    save_to_json_file(py_list, filename)
+    my_list = load_from_json_file(filename)
+except:
+    my_list = []
+for x in range(1, len(sys.argv)):
+    my_list.append(sys.argv[x])
+save_to_json_file(my_list, filename)
