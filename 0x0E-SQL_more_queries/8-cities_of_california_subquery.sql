e2458b533a88d5bcba56af2b35a9b78cec002c06
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2021-11-18 01:43:15 +0200

    fixed sub query

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,5 +3,5 @@
 USE hbtn_0d_usa;
 SELECT id, name FROM cities
 WHERE state_id = (SELECT id FROM states
-                  WHERE name = "California")
+                  WHERE name = 'California')
 ORDER BY cities.id ASC;
