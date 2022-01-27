b4e38144de0d91cceb02f8a343b05f81d127e6b2
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-01-27 23:32:39 +0200

    Added 8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const args = process.argv;
+if (isNaN(parseInt(args[2]))) {
+  console.log('Missing number of occurrences');
+} else {
+  let i, j, string;
+  for (i = 0; i < parseInt(args[2]); i++) {
+    string = '';
+    for (j = 0; j < parseInt(args[2]); j++) {
+      string += 'X';
+    }
+    console.log(string);
+  }
+}
