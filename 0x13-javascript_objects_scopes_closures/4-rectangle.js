4adbf73788de2a911b3375e967055f9428c29fca
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-03-26 11:32:58 +0200

    Fixed trailing whitespaces

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }process.stdout.write('\n');
+    }
+  }
+
+  rotate () {
+    const w = this.width;
+    const h = this.height;
+    this.width = h;
+    this.height = w;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}module.exports = Rectangle;
