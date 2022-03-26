562f4faaa552cbd2fe23614cefdfd50135688e1a
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-03-26 11:57:55 +0200

    Fixed caracter X

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Square1 = require('./5-square');
+class Square extends Square1 {
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write('X');
+        }process.stdout.write('\n');
+      }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write(c);
+        }process.stdout.write('\n');
+      }
+    }
+  }
+}module.exports = Square;
