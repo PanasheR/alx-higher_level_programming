557713a7bd3ce014d0fa647c8998b643a621159e
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-03-27 02:28:49 +0200

    Fixed semistandard style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,5 @@
 const fis = require('fs');
 const arg = process.argv[2];
 fis.readFile(arg, 'utf-8', (err, result) => {
-  if (err) { console.log(err); }
-  else { console.log(result); }
+  if (err) { console.log(err); } else { console.log(result); }
 });
