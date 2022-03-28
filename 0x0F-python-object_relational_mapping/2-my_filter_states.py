373a616bca09e66bddc9bd1ff7f537336ed6259f
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-03-28 18:04:18 +0200

    Fixed pycodestyle tests

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 """Display state values that match argument"""
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
 
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=argv[1],
-                           passwd=argv[2],
-                           db=sys.argv[3])
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name='{:s}' ORDER BY id ASC"
-                .format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{:s}'".
+                format(argv[4]))
+
     rows = cur.fetchall()
+
     for row in rows:
         print(row)
+
     cur.close()
-    conn.close()
+    db.close()
