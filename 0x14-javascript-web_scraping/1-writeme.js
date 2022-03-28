b92b885eaafaec8d2519e38069d86a1ade87c874
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2022-03-28 15:27:00 +0200

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/1-writeme.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const fis = require('fs');
-const arg = process.argv;
-fis.writeFile(arg[2], arg[3], 'utf-8', (err, data) => {
-  if (err) { console.log(err); }
-});
--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const requesting = require('request');
-const arg = process.argv;
-requesting(arg[2], function (error, response, body) {
-  if (error) { console.log(error); }
-  console.log('code:', response && response.statusCode);
-});
