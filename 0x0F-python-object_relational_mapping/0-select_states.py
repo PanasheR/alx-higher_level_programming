f8f2a3fa3480ca33cf925d0515bf0f1f0df72685
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-03-28 17:20:32 +0200

    Fixed pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,10 +4,11 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost"
-		        ,port=3306
-			,user=sys.argv[1]
-		        ,passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT DISTINCT * FROM states ORDER BY id")
     rows = cur.fetchall()
@@ -15,4 +16,3 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
-
