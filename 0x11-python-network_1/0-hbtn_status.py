571df841d0d7679f56ce573063092e541e274c5b
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-04-02 12:53:47 +0200

    What is my status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# script that fetches https://alx-intranet.hbtn.io/status
+from urllib.request import urlopen
+
+if __name__ == "__main__":
+    with urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
