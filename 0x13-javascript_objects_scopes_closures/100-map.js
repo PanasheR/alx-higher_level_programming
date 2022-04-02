6a8eed0d3cfa63676cf276da10c837bde5353ba7
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-04-02 14:21:16 +0200

    Removed advanced files

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-// script that imports an array but file 100-data is absent
-const set = require('./100-data').list;
-console.log(set);
-let count = 0;
-const mapping = set.map(function (x) { return (x * count++); });
-console.log(mapping);
