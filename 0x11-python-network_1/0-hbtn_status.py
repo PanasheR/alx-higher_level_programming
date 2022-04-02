69db6cc679b97a32fb3d2489c08c4d1b15c37b22
Panashe Rusakaniko <71722226+PanasheR@users.noreply.github.com>
2022-04-02 13:19:15 +0200

    What is my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@
 if __name__ == "__main__":
     from urllib.request import urlopen
 
-    with urlopen('http://0.0.0.0:5050/status') as response:
+    with urlopen('https://alx-intranet.hbtn.io/status') as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
